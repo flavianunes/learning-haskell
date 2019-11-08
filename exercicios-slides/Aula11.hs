bd1a9b02211f89dd3c9aa80fe949dcd14dcb7397
Flavia Nunes <flavianunesufop@gmail.com>
2019-11-08 10:33:50 -0300

    aula 11

--- exercicios-slides/Aula11.hs
+++ exercicios-slides/Aula11.hs
@@ -1,14 +1,51 @@
 module Aula11 where
-    --distancia entre dois pontos
+
+    --PONTO
     data Point = Point Float Float
 
+    -- calculando a norma de um ponto
+    norm :: Point -> Float
+    norm (Point x y) = sqrt (x^2 + y^2)
+
+    --calculando distancia
     distance :: Point -> Point -> Float
-    distance (Point x1 y1) (Point x2 y2) = sqrt ((x1-x2)^2 + (y1-y2)^2)
+    distance (Point x y) (Point w z) = sqrt(a + b)
+        where
+            a = (x - w)^2
+            b = (y - z)^2
 
-    data Shape = Rect Point Float Float
-                | Cirle Point Float
-                | Triangle Point Point Point
-
+    --FORMAS
+    data Shape = Circle Point Float | Rectangle Point Float Float  
+    --calculando area
     area :: Shape -> Float
-    area (Rect p w h) = w * r
-    area (Circle p r) =  2 * r^2
+    area (Rectangle p w h) = w * h
+    area (Circle p r) = 2 * r^2
+
+    -- LISTAS E ARVORES
+    data IntList = Empty
+             | Ins Int IntList
+
+    data IntTree = Leaf
+             | Node Int IntTree IntTree
+
+    -- busca sequencial na lista
+    elemList :: Int -> IntList -> Bool
+    elemList _ Empty = False
+    elemList n (Ins m xs) = n == m || elemList n xs
+
+    -- TIPOS POLIMÓRFICOS
+    data TRee a = TLeaf
+                | Tnote a (Tree a) (Tree b)
+
+    -- TIPO MAYBE
+    data Maybe a = Just a | Nothing
+    --The Maybe type encapsulates an optional value. A value of type Maybe a either contains a value of type a (represented as Just a), or it is empty (represented as Nothing). Using Maybe is a good way to deal with errors or exceptional cases without resorting to drastic measures such as error.
+
+    -- SAFEHEAD
+    head :: [a] -> a
+    head [] = error "Prelude.head: empty list"
+    head (x : _) = x
+
+    safeHead :: [a] -> Maybe a
+    safeHead [] = Nothing
+    safeHead (x : _) = Just x
